daa29799e9ef70eca0436f6cdfb9e0b5ab62ffe6
Victor Manuel <vicmandlagasca@gmail.com>
2020-02-21 01:44:28 -0500

    recoveyr passwords

--- roles/urls.py
+++ roles/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse, reverse_lazy
+from decouple import config
 
 from .views import (
     SignUpView, SellerSignUpView,
@@ -21,5 +23,31 @@ urlpatterns = [
     ), name = "change_password"),
     path('cambiar-password/exitoso', auth_views.PasswordChangeView.as_view(
         template_name="scolarte/registration/change_password_success.html"        
-    ), name = "change_password_success")
+    ), name = "change_password_success"),
+    # Path for recovery passwords
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='scolarte/registration/password_reset.html',
+             subject_template_name='scolarte/registration/password_reset_subject.txt',
+             from_email=config('FROM_EMAIL'),
+             email_template_name='scolarte/registration/password_reset_email.html',
+             success_url= reverse_lazy('roles:password_reset_done')
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='scolarte/registration/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='scolarte/registration/password_reset_confirm.html',
+             success_url= reverse_lazy('roles:password_reset_complete')
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='scolarte/registration/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
