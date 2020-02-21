339749127264ca0c7f61adf979ee65c23c4e0e82
Victor Manuel <vicmandlagasca@gmail.com>
2020-02-20 23:24:07 -0500

    Recovery password

--- roles/urls.py
+++ roles/urls.py
@@ -16,4 +16,8 @@ urlpatterns = [
     path('registro/clientes', MyClientSignupView, name='client_signup'),
     path('perfil/', update_client_profile, name='profile_update'),
     path('password-reset', auth_views.PasswordResetView.as_view(template_name="scolarte/registration/password_reset.html"), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name="scolarte/registration/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('password_reset_complete/$',auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
